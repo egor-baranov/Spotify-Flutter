b9c69d868c723377c076a30e628c6810d2b00749
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-05-15 21:10:43 +0300

    🎉 Initial commit

---
+++ android/app/src/main/kotlin/com/kepler88d/spotify_flutter/spotify_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kepler88d.spotify_flutter.spotify_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SpotifyApp());
+}
+
+class SpotifyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App Demo Build',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MainPage(title: 'Spotify Flutter Custom Client'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  MainPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content:
+                      Text("Yes, I should track this action after some time"),
+                ));
+              },
+              child: Text(
+                'Press here to see something',
+                style: TextStyle(color: Colors.white),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
