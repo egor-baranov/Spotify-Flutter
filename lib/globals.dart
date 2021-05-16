56029f7f89b0cbbd0d07a8d9885b46712d2c140a
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-05-16 03:40:30 +0300

    ✨ Green and black spotify colors been added to globals.dart file

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,1 +1,38 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
 const client_id = "9d2635f02f6f4975bfb722d594cbaa27";
+const redirect_uri = "spotifyflutter:/";
+
+Map<int, Color> spotifyGreenColorMap = {
+  50: Color.fromRGBO(30, 215, 96, .1),
+  100: Color.fromRGBO(30, 215, 96, .2),
+  200: Color.fromRGBO(30, 215, 96, .3),
+  300: Color.fromRGBO(30, 215, 96, .4),
+  400: Color.fromRGBO(30, 215, 96, .5),
+  500: Color.fromRGBO(30, 215, 96, .6),
+  600: Color.fromRGBO(30, 215, 96, .7),
+  700: Color.fromRGBO(30, 215, 96, .8),
+  800: Color.fromRGBO(30, 215, 96, .9),
+  900: Color.fromRGBO(30, 215, 96, 1),
+};
+
+MaterialColor spotifyGreenColor =
+    MaterialColor(0xFF1DB954, spotifyGreenColorMap);
+
+Map<int, Color> spotifyBlackColorMap = {
+  50: Color.fromRGBO(25, 20, 20, .1),
+  100: Color.fromRGBO(25, 20, 20, .2),
+  200: Color.fromRGBO(25, 20, 20, .3),
+  300: Color.fromRGBO(25, 20, 20, .4),
+  400: Color.fromRGBO(25, 20, 20, .5),
+  500: Color.fromRGBO(25, 20, 20, .6),
+  600: Color.fromRGBO(25, 20, 20, .7),
+  700: Color.fromRGBO(25, 20, 20, .8),
+  800: Color.fromRGBO(25, 20, 20, .9),
+  900: Color.fromRGBO(25, 20, 20, 1),
+};
+
+MaterialColor spotifyBlackColor =
+    MaterialColor(0xFF191414, spotifyBlackColorMap);
