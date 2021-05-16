18d8ad65f63ed0f6ee396a9e5bf856ac00173a4f
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-05-16 03:42:24 +0300

    🎨 Big ui improvements and button authentication callback

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
+import 'dart:async';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_web_auth/flutter_web_auth.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'globals.dart' as globals;
 
 void main() {
@@ -12,7 +18,8 @@ class SpotifyApp extends StatelessWidget {
     return MaterialApp(
       title: 'App Demo Build',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        fontFamily: 'Nunito',
+        primarySwatch: globals.spotifyGreenColor,
       ),
       home: MainPage(title: 'Spotify Flutter Custom Client'),
       debugShowCheckedModeBanner: false,
@@ -33,28 +40,69 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                  content:
-                      Text("Yes, I should track this action after some time"),
-                ));
-              },
-              child: Text(
-                'Press here to see something',
-                style: TextStyle(color: Colors.white),
-              ),
-            )
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              "Hey.\nThis is a custom Spotify client made using Flutter.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: globals.spotifyBlackColor,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w900,
+                  fontFamily: 'Nunito'),
+            ),
+            Divider(height: 50, color: Colors.transparent),
+            Container(
+              alignment: Alignment.bottomCenter,
+              padding: EdgeInsets.all(32),
+              child: SizedBox(
+                  width: double.infinity,
+                  height: 60,
+                  child: ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        elevation: 8,
+                        shadowColor: globals.spotifyGreenColor,
+                        shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(28),
+                        )),
+                    onPressed: () {
+                      getAuthenticationToken();
+                    },
+                    label: Text(
+                      'Sign in with Spotify',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    icon: Container(
+                        padding: const EdgeInsets.all(12),
+                        child: Image.asset(
+                          "assets/spotify_icon.png",
+                        )),
+                  )),
+            ),
+            Divider(height: 200, color: Colors.transparent),
           ],
         ),
       ),
     );
+  }
+
+  void getAuthenticationToken() async {
+    final result = await FlutterWebAuth.authenticate(
+      url: "https://accounts.spotify.com/authorize?" +
+          "client_id=${globals.client_id}&" +
+          "redirect_uri=${globals.redirect_uri}&" +
+          "scope=user-read-currently-playing&" +
+          "response_type=token&" +
+          "state=123",
+      callbackUrlScheme: "spotifyflutter",
+    );
+
+    var accessToken = Uri.parse(result).queryParameters['token'];
+    print(accessToken);
   }
 }
